4f0d271fe6ff40afa2d3838c62bf92a76fd35044
bp96 <brijeshpatel96@gmail.com>
2021-12-27 13:30:02 +0000

    updating localhost references

--- face-recognition-brain/src/components/Register/Register.js
+++ face-recognition-brain/src/components/Register/Register.js
@@ -29,7 +29,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/register', {
+    fetch('https://brijeshpatel.co.uk/register', { // URL: http://localhost:3001/register
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
